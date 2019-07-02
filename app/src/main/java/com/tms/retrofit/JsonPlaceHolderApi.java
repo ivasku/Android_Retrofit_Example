abe5f378dc35883ad87493440990d192d2f0d6de
Adrian Ivasku <a.ivasku@levi9.com>
2019-07-02 14:11:33 +0200

    added more use cases

--- app/src/main/java/com/tms/retrofit/JsonPlaceHolderApi.java
+++ app/src/main/java/com/tms/retrofit/JsonPlaceHolderApi.java
@@ -1,9 +1,14 @@
 package com.tms.retrofit;
 
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Url;
 
 public interface JsonPlaceHolderApi {
     //full page url
@@ -16,5 +21,36 @@ public interface JsonPlaceHolderApi {
     // we put annotation with the name of the page we want
     @GET ("posts")
     Call<List<Post>> getPosts();
+
+
+   // @GET ("posts/2/comments")
+   // Call<List<Comments>> getComments();
+
+    //To avoid hardcoding the id 2, we can pass it as a argument
+    @GET ("posts/{id}/comments")
+    Call<List<Comments>> getComments(@Path("id") int postId);
+
+
+    //get posts via querry
+    @GET ("posts")
+    Call<List<Post>> getPostsQuerry(@Query("userId") int userId);
+
+    //get posts via querry with multiple arguments
+    @GET ("posts")
+    Call<List<Post>> getPostsQuerryMul(
+            @Query("userId") int userId,
+            @Query("_sort") String sort,
+            @Query("_order") String order);
+
+
+    //get posts via querry with multiple arguments but define arguments when we call it
+    @GET ("posts")
+    Call<List<Post>> getPostsQuerryMulCustom(
+            @QueryMap Map<String, String> param);
+
+    //pass the whole URL if we have complicated structure
+    @GET
+    Call<List<Comments>> getCommentsCustom(@Url String url);
+
 
 }
--- app/src/main/java/com/tms/retrofit/MainActivity.java
+++ app/src/main/java/com/tms/retrofit/MainActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -18,6 +20,10 @@ public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private TextView result;
 
+    private JsonPlaceHolderApi jsonPlaceHolderApi;
+    private Retrofit retrofit;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,17 +33,32 @@ public class MainActivity extends AppCompatActivity {
 
         // we create retrifut object with static main URL
         // and we specify the JSON converter we use
-        Retrofit retrofit = new Retrofit.Builder()
+        retrofit = new Retrofit.Builder()
                 .baseUrl("https://jsonplaceholder.typicode.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         //now we can create out API where the Retrofit will implement the functions found in this interface
-        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+
+        //getPosts();
+
+        //getComments();
+
+        //getPostsQuerry();
+
+        //getPostsQuerryMul();
+
+        //getPostsQuerryMulCustom();
+
+        getCommentsCustom();
+
+    }
+
+    private void getPosts() {
 
         // to execute our network request we use Call object
         Call<List<Post>> call = jsonPlaceHolderApi.getPosts();
-
 
         // we call enque to run on backround thread
         call.enqueue(new Callback<List<Post>>() {
@@ -72,6 +93,208 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "Error in call enque" + t.getLocalizedMessage());
                 Log.d(TAG, "Error in call enque" + t.getMessage());
                 Log.d(TAG, "Error in call enque" + t.getCause());
+            }
+        });
+    }
+
+    private void getComments() {
+
+        Call<List<Comments>> commentsCall = jsonPlaceHolderApi.getComments(3);
+
+        commentsCall.enqueue(new Callback<List<Comments>>() {
+            @Override
+            public void onResponse(Call<List<Comments>> call, Response<List<Comments>> response) {
+                if (!response.isSuccessful()) {
+                    result.setText("Code: " + response.code());
+                    return;
+                }
+
+                List<Comments> comments = response.body();
+
+                for (Comments com : comments) {
+                    String content = "";
+                    content += "ID: " + com.getId() + "\n";
+                    content += "postId: " + com.getPostId()+ "\n";
+                    content += "name: " + com.getName()+ "\n";
+                    content += "mail: " + com.getMail()+ "\n";
+                    content += "body: " + com.getBody()+ "\n\n";
+
+                    result.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Comments>> call, Throwable t) {
+                Log.d(TAG, "Error in call enque comments" + t.getLocalizedMessage());
+                Log.d(TAG, "Error in call enque comments" + t.getMessage());
+                Log.d(TAG, "Error in call enque comments" + t.getCause());
+            }
+        });
+    }
+
+    private void getPostsQuerry() {
+
+        // to execute our network request we use Call object
+        Call<List<Post>> call = jsonPlaceHolderApi.getPostsQuerry(4);
+
+        // we call enque to run on backround thread
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+
+                // if response is not 404
+                // response should be something between 200 and 300
+                // we check if the response is ok, if it is not, simply return
+                if (!response.isSuccessful()) {
+                    result.setText("Code: " + response.code());
+                    return;
+                }
+
+                // we get our list of posts, every Json object
+                List<Post> post = response.body();
+
+                for (Post data: post) {
+
+                    String content = "";
+                    content += "ID: " + data.getId() + "\n";
+                    content += "User ID: " + data.getUserId() + "\n";
+                    content += "Title: " + data.getTitle() + "\n";
+                    content += "Text: " + data.getText() + "\n\n";
+
+                    result.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                Log.d(TAG, "Error in call enque" + t.getLocalizedMessage());
+                Log.d(TAG, "Error in call enque" + t.getMessage());
+                Log.d(TAG, "Error in call enque" + t.getCause());
+            }
+        });
+    }
+
+    private void getPostsQuerryMul() {
+
+        // to execute our network request we use Call object
+        Call<List<Post>> call = jsonPlaceHolderApi.getPostsQuerryMul(4, "id", "desc");
+
+        // we call enque to run on backround thread
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+
+                // if response is not 404
+                // response should be something between 200 and 300
+                // we check if the response is ok, if it is not, simply return
+                if (!response.isSuccessful()) {
+                    result.setText("Code: " + response.code());
+                    return;
+                }
+
+                // we get our list of posts, every Json object
+                List<Post> post = response.body();
+
+                for (Post data: post) {
+
+                    String content = "";
+                    content += "ID: " + data.getId() + "\n";
+                    content += "User ID: " + data.getUserId() + "\n";
+                    content += "Title: " + data.getTitle() + "\n";
+                    content += "Text: " + data.getText() + "\n\n";
+
+                    result.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                Log.d(TAG, "Error in call enque" + t.getLocalizedMessage());
+                Log.d(TAG, "Error in call enque" + t.getMessage());
+                Log.d(TAG, "Error in call enque" + t.getCause());
+            }
+        });
+    }
+
+    private void getPostsQuerryMulCustom() {
+
+        Map<String, String> querryMap = new HashMap<>();
+        querryMap.put("userId", "1");
+        querryMap.put("_sort", "id");
+        querryMap.put("_order", "desc");
+
+
+        // to execute our network request we use Call object
+        Call<List<Post>> call = jsonPlaceHolderApi.getPostsQuerryMulCustom(querryMap);
+
+        // we call enque to run on backround thread
+        call.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(Call<List<Post>> call, Response<List<Post>> response) {
+
+                // if response is not 404
+                // response should be something between 200 and 300
+                // we check if the response is ok, if it is not, simply return
+                if (!response.isSuccessful()) {
+                    result.setText("Code: " + response.code());
+                    return;
+                }
+
+                // we get our list of posts, every Json object
+                List<Post> post = response.body();
+
+                for (Post data: post) {
+
+                    String content = "";
+                    content += "ID: " + data.getId() + "\n";
+                    content += "User ID: " + data.getUserId() + "\n";
+                    content += "Title: " + data.getTitle() + "\n";
+                    content += "Text: " + data.getText() + "\n\n";
+
+                    result.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Post>> call, Throwable t) {
+                Log.d(TAG, "Error in call enque" + t.getLocalizedMessage());
+                Log.d(TAG, "Error in call enque" + t.getMessage());
+                Log.d(TAG, "Error in call enque" + t.getCause());
+            }
+        });
+    }
+
+    private void getCommentsCustom() {
+
+        Call<List<Comments>> commentsCall = jsonPlaceHolderApi.getCommentsCustom("posts/2/comments");
+
+        commentsCall.enqueue(new Callback<List<Comments>>() {
+            @Override
+            public void onResponse(Call<List<Comments>> call, Response<List<Comments>> response) {
+                if (!response.isSuccessful()) {
+                    result.setText("Code: " + response.code());
+                    return;
+                }
+
+                List<Comments> comments = response.body();
+
+                for (Comments com : comments) {
+                    String content = "";
+                    content += "ID: " + com.getId() + "\n";
+                    content += "postId: " + com.getPostId()+ "\n";
+                    content += "name: " + com.getName()+ "\n";
+                    content += "mail: " + com.getMail()+ "\n";
+                    content += "body: " + com.getBody()+ "\n\n";
+
+                    result.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Comments>> call, Throwable t) {
+                Log.d(TAG, "Error in call enque comments" + t.getLocalizedMessage());
+                Log.d(TAG, "Error in call enque comments" + t.getMessage());
+                Log.d(TAG, "Error in call enque comments" + t.getCause());
             }
         });
     }
